ab13ee9190a206017d0341081c13f506ec53def4
Yuri Lima <yurilima742@yahoo.com>
2024-01-11 10:21:45 -0300

    feat: aula 02 de condicionais e resto da divisão

---
+++ aula_02.js
@@ -0,0 +1,13 @@
+const camisaAzul = false;
+const num = 10;
+const isPar = (num % 2) === 0;
+
+if(num == 0) {
+    console.log("Número inválido");
+} else if (isPar) {
+    console.log(isPar);
+    console.log("É número par");
+} else {
+    console.log(isPar);
+    console.log("É número impar");
+}
