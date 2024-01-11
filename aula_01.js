971efb88107a133b82892edef836e0ff68374b50
Yuri Lima <yurilima742@yahoo.com>
2024-01-11 09:32:10 -0300

    feat: aula 01

---
+++ aula_01.js
@@ -0,0 +1,12 @@
+let variavel = 10 + 10;
+
+const pi = 3.14;
+
+console.log(variavel)
+console.log(pi)
+
+variavel = 30 * 3;
+console.log(variavel)
+
+variavel = variavel / 4;
+console.log(variavel)
