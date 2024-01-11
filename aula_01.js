6b0b8483b9d450594e44c3937ded4f02a3f7526e
Yuri Lima <yurilima742@yahoo.com>
2024-01-11 09:58:05 -0300

    refactor: add ponto e virgula na aula 01

--- aula_01.js
+++ aula_01.js
@@ -2,11 +2,11 @@ let variavel = 10 + 10;
 
 const pi = 3.14;
 
-console.log(variavel)
-console.log(pi)
+console.log(variavel);
+console.log(pi);
 
 variavel = 30 * 3;
-console.log(variavel)
+console.log(variavel);
 
 variavel = variavel / 4;
-console.log(variavel)
+console.log(variavel);
