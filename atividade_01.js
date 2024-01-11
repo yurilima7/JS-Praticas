d277abc7428ab4f9bae0411502a7a52a99bf4a38
Yuri Lima <yurilima742@yahoo.com>
2024-01-11 09:56:32 -0300

    feat: atividade 01

---
+++ atividade_01.js
@@ -0,0 +1,8 @@
+// Calcular o valor de uma viagem
+const fuelPrice = 4.50;
+const kmPerLiter = 10;
+const distanceTraveled = 500;
+
+let finalSpending = (distanceTraveled / kmPerLiter) * fuelPrice;
+
+console.log(finalSpending);
