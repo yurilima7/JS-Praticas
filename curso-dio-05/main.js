48664b8db769012e57d061733b21f0ec28debee2
Yuri Lima <yurilima742@yahoo.com>
2024-01-16 10:07:10 -0300

    feat: aulas curso 5

---
+++ curso-dio-05/main.js
@@ -0,0 +1,9 @@
+const functions = require('./functions');
+
+const {gets, print} = require('./functions'); // Object Destructuring
+
+console.log(functions.gets());
+functions.print('Armand');
+
+console.log(gets());
+print('Armand');
